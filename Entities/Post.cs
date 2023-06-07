31dc19bcee7a6e84ed744a5d30442963c0778351
JuanAsbahr <juan_asbahr@hotmail.com>
2023-06-07 18:39:38 -0300

    Program

--- Entities/Post.cs
+++ Entities/Post.cs
@@ -35,5 +35,21 @@ namespace SocialAppLogic.Entities
         {
             Comments.Remove(comment);
         }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(Title);
+            sb.Append(Likes);
+            sb.Append(" Likes - ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.AppendLine(Content);
+            sb.AppendLine("Comments: ");
+            foreach (Comment c in Comments)
+            {
+                sb.AppendLine(c.Text);
+            }
+            return sb.ToString();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using SocialAppLogic.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Comment c1 = new Comment("Have a nice trip!");
+        Comment c2 = new Comment("Wow that's awesoma!");
+        Post p1 = new Post(
+            DateTime.Parse("07/06/2023  18:26:38"),
+            "Treveling to New Zealand",
+            "I'm goig to visit this wonderful country!",
+            12);
+
+        p1.AddComment (c1);
+        p1.AddComment (c2);
+
+        Comment c3 = new Comment("Good night!");
+        Comment c4 = new Comment("May the Force be with you");
+        Post p2 = new Post(
+            DateTime.Parse("05/06/2023 20:45:53"),
+            "Good night guys",
+            "See you tomorrow",
+            5);
+
+        p2.AddComment (c3);
+        p2.AddComment (c4);
+
+        Console.WriteLine(p1);
+        Console.WriteLine(p2);
+    }
+}
